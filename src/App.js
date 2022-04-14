bc0211cd9120b684901256095fe8aace5f750385
kstaver <staverkendra@gmail.com>
2022-04-13 20:43:46 -0700

    added narbar to app

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import Volunteer from './components/Volunteer/Volunteer';
 import Events from './components/Events/Events';
 import Donate from './components/Donate/Donate';
 import {Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/About" element={<About/>}/>
