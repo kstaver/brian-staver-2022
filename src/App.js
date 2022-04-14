ae4d8dc1d866fb18ed83ab8f4442fd3d73d9e129
kstaver <staverkendra@gmail.com>
2022-04-13 20:08:35 -0700

    created folders for each page and moved each page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Endorsements from './components/Endorsements';
-import Petition from './components/Petition';
-import Platform from './components/Platform';
-import Volunteer from './components/Volunteer';
-import Events from './components/Events';
-import Donate from './components/Donate';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Endorsements from './components/Endorsements/Endorsements';
+import Petition from './components/Petition/Petition';
+import Platform from './components/Platform/Platform';
+import Volunteer from './components/Volunteer/Volunteer';
+import Events from './components/Events/Events';
+import Donate from './components/Donate/Donate';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
