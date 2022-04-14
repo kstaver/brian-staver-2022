92c67bac4330a704bc43ff0a23c5c73990c71f0b
kstaver <staverkendra@gmail.com>
2022-04-13 20:15:41 -0700

    added css files for each page & imported css to js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutStyle from './About.css';
 
 function About() {
   return (
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HomeStyle from './Home.css';
 
 function Home() {
   return (
