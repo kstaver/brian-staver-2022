5757a0fdaf6e3ba57e6ed66b29fc5a64f1a840d9
kstaver <staverkendra@gmail.com>
2022-04-19 01:14:18 -0700

    added Links to each url: element in the array

--- src/components/NavBar/MenuItems.js
+++ src/components/NavBar/MenuItems.js
@@ -1,47 +1,59 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Home from '../Body/Home/Home';
+import About from '../Body/About/About';
+import Contact from '../Body/Contact/Contact';
+import Endorsements from '../Body/Endorsements/Endorsements';
+import Petition from '../Body/Petition/Petition';
+import Platform from '../Body/Platform/Platform';
+import Volunteer from '../Body/Volunteer/Volunteer';
+import Events from '../Body/Events/Events';
+import Donate from '../Body/Donate/Donate';
+
 export const MenuItems = [
     {
         title:'Home',
-        url: '#',
+        url: <Link to={<Home/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'About',
-        url: '#',
+        url:  <Link to={<About/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Contact',
-        url: '#',
+        url:  <Link to={<Contact/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Endorsements',
-        url: '#',
+        url:  <Link to={<Endorsements/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Events',
-        url: '#',
+        url:  <Link to={<Events/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Petition',
-        url: '#',
+        url:  <Link to={<Petition/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Platform',
-        url: '#',
+        url:  <Link to={<Platform/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Volunteer',
-        url: '#',
+        url:  <Link to={<Volunteer/>}></Link>,
         cName: 'nav-links',
     },
     {
         title:'Donate',
-        url: '#',
+        url:  <Link to={<Donate/>}></Link>,
         cName: 'nav-links-mobile',
     },
 ]
