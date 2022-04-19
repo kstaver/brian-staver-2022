a2d1202e5df10ddee54b17e208737f04360b045c
kstaver <staverkendra@gmail.com>
2022-04-19 00:08:39 -0700

    added navbar

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,47 @@
+const MenuItems = [
+    {
+        title:'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'About',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Contact',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Endorsements',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Events',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Petition',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Platform',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Volunteer',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title:'Donate',
+        url: '#',
+        cName: 'nav-links',
+    },
+]
