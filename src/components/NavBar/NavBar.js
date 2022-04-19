d937b3aed2e70cbe5a55e4d4b9bc21ff602059a2
kstaver <staverkendra@gmail.com>
2022-04-18 19:16:59 -0700

    working on a navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import NavBarStyle from './NavBar.css';
+import './NavBar.css';
 
 function NavBar() {
   return (
@@ -34,6 +34,35 @@ function NavBar() {
         </li>
     </ul>
   );
+}
+
+export default NavBar;
+
+
+export default NavBar;
+import React, { useState } from 'react';
+import "./header.css";
+import Mobile from './mobile/index';
+import Web from './web/index';
+
+function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="header">
+            <div className="logo">Kendra's Portfolio</div>
+            <div className="menu">
+                <div className="web-menu">
+                    <Web />
+                </div>
+                <div className="mobile-menu">
+                    <div onClick={() => setIsOpen(!isOpen)}>
+                    <img src={process.env.PUBLIC_URL + '/images/menu-32.png'} alt="icon" />
+                    </div>
+                    {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NavBar;
