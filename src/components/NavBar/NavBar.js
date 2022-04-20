e9aa78f01d2ed2feaf3f87d0f622631c52d80883
kstaver <staverkendra@gmail.com>
2022-04-20 00:47:24 -0700

    messing around with onClick

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Navbar.css';
 import { MenuItems } from "./MenuItems";
 
-function Navbar() {
-  return (
-    <nav className='NavbarItems'>
-        <h1 className='navbar-logo'>Brian Staver<i className='fab fa-react'></i></h1>
-        <div className='menu-icon'>
-            <i class="fa-solid fa-square-list"></i>
-        </div>
-        <ul>
-            {MenuItems.map((item, index)=>{
-                return (
-                    <li key={index}>
-                        <a className={item.cName} href={item.url}>
-                            {item.title}
-                        </a>
-                    </li>
-                )
-            })}
-        </ul>
-    </nav>
-  );
+class Navbar extends Component{
+   // state = { clicked:false }
+   
+    render(){
+        return (
+            <nav className='NavbarItems'>
+                <h1 className='navbar-logo'>Brian Staver</h1>
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className="fa-solid fa-square-list"></i>
+                </div>
+                <ul>
+                    {MenuItems.map((item, index)=>{
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        );
+    }
 }
 
 export default Navbar;
